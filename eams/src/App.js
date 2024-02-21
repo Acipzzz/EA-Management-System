d43c581457a39d5ad730fc597a76ea386dc4ec12
ayiesyahir <ayiesyahir@gmail.com>
2024-02-21 22:09:42 +0800

    Test

--- eams/src/App.js
+++ eams/src/App.js
@@ -1,15 +1,16 @@
 //import logo from './logo.svg';
 import React from 'react';
-//import './App.css';
+import './App.css';
+import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import BorangAduan from './pages/BorangAduan';
 import PenginapanHotel from './pages/PenginapanHotel';
-//import PenginapanWisma from './pages/PenginapanWisma';
-//import PerancanganPenginapan from './pages/PerancanganPenginapan';
-//import StatusKerosakan from './pages/StatusKerosakan';
+import PenginapanWisma from './pages/PenginapanWisma';
+import PerancanganPenginapan from './pages/PerancanganPenginapan';
+import StatusKerosakan from './pages/StatusKerosakan';
 import ErrorPage from './pages/Error';
 import {Route, Routes} from 'react-router-dom';
-import NavBar from './components/NavBar';
+
 
 
 
@@ -21,6 +22,9 @@ function App() {
         <Route exact path='/' element={<Home />} />
         <Route path='/borang-aduan' element={<BorangAduan />} />
         <Route path='/penginapan-hotel' element={<PenginapanHotel />} />
+        <Route path='/penginapan-wisma' element={<PenginapanWisma />} />
+        <Route path='/penginapan-penginapan' element={<PerancanganPenginapan />} />
+        <Route path='/status-kerosakan' element={<StatusKerosakan />} />
         <Route element={<ErrorPage />} />
       </Routes>
     </>
